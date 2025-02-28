1e75da8e133ecfcfcab37402e30bb271a9cf3d9d
harveyjae <1031964705@qq.com>
2025-02-28 20:11:40 +0800

    实现SBI的UART接口

---
+++ kernel/dev/include/uart.h
@@ -0,0 +1,10 @@
+#ifndef __DEV_UART__H__
+#define __DEV_UART__H__
+
+#include "common/types.h"
+#include "sbi.h"
+#define USE_QEMU_VIRT
+void uart_putchar(int64_t ch);
+int64_t uart_getchar(void);
+void uart_init(void);
+#endif  /* !__DEV_UART__H__ */
---
+++ kernel/dev/src/uart.c
@@ -0,0 +1,39 @@
+#include "uart.h"
+#include "printf.h"
+#include "sbi.h"
+
+
+#ifdef  USE_QEMU_VIRT
+void uart_putchar(int64_t ch)
+{
+    sbi_putchar(ch);
+}
+int64_t uart_getchar(void)
+{
+    return sbi_getchar().value;
+}
+void uart_init(void)
+{
+    /* 当使用QEMU的时候,初始化函数什么都不需要做 */
+    /* do nothing*/
+    /* 下面的代码仅测试用 */
+    uart_putchar('U');
+    uart_putchar('A');
+    uart_putchar('R');
+    uart_putchar('T');
+    uart_putchar(' ');
+    uart_putchar('I');
+    uart_putchar('n');
+    uart_putchar('i');
+    uart_putchar('t');
+    uart_putchar(' ');
+    uart_putchar('S');
+    uart_putchar('U');
+    uart_putchar('C');
+    uart_putchar('C');
+    uart_putchar('E');
+    uart_putchar('S');
+    uart_putchar('S');
+    uart_putchar('\n');
+}
+#endif  //USE_QEMU_VIRT
